bd844582253eae83bbe1572d73e64e07c2f5ddd0
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-06 11:32:08 -0500

    Test development

---
+++ app.py
@@ -0,0 +1,55 @@
+# App.py (testing bucket list tutorial)
+
+from flask import Flask, render_template, request, json, session, redirect
+from flaskext.mysql import MySQL
+import bcrypt
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5001, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
