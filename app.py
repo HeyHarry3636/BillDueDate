244f3d7fb5e16c4463ffd6b8d024090cfad359a8
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-07 07:03:15 -0500

    Test development

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return "Welcome!"
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
