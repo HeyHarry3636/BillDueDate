8513274dd0bad77ac5c4677c4f255d7a34448bf6
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-07 12:14:59 -0500

    Test development

--- sql/BDD_sp_createUser.sql
+++ sql/BDD_sp_createUser.sql
@@ -8,3 +8,23 @@ CREATE PROCEDURE sp_createUser
 	IN p_email VARCHAR(60),
 	IN p_password BINARY(60)
 )
+BEGIN
+	IF (SELECT EXISTS (SELECT 1 FROM tbl_user WHERE user_email = p_email)) THEN
+		SELECT 'Email has already been registered!';
+	ELSE
+		INSERT INTO tbl_user
+		(
+			user_email,
+			user_password,
+			user_createdDate
+		)
+		VALUES
+		(
+			p_email,
+			p_password,
+			NOW()
+		);
+	END IF;
+END;
+$$
+DELIMITER ;
