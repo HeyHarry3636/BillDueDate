9dc4b76eb45fa0e841cdc59c8190bf038d4317df
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-07 12:26:18 -0500

    Test development

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def main():
 	return render_template('index.html')
+	
+@app.route("showSignUp")
+def showSignUp():
+	return render_template('signUp.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000, debug=True)
