0ddfe00d2e9179008618a0bfdd8c90115c33b7a7
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-07 09:01:18 -0500

    Test development

--- sql/BDD_populateTables.sql
+++ sql/BDD_populateTables.sql
@@ -6,3 +6,18 @@
 INSERT INTO tbl_user(user_id, user_email, user_password, user_createdDate)
 	VALUES(1, "Test@Test.com", "Test", NOW());
 
+-- Populate RECUR table
+INSERT INTO tbl_recur(recur_id, recur_interval, recur_value)
+	VALUES(1, "Monthly", 30);
+
+-- Populate BANK table
+INSERT INTO tbl_bank(bank_id, user_id, bank_currentAmount, bank_payDayAmount, recur_id)
+	VALUES(1, 1, 1234.56, 1000.00, 1);
+
+-- Populate BILL table
+INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_date, recur_id, bill_createdDate, bill_paid)
+	VALUES(1, 1, "Fed Loans", "Income-Based", 199.70, 06/01/2018, 1, NOW(), 1);
+INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_date, recur_id, bill_createdDate, bill_paid)
+	VALUES(1, 1, "Public Storage", NULL, 118.00, 06/01/2018, 1, NOW(), 1);
+INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_date, recur_id, bill_createdDate, bill_paid)
+	VALUES(1, 1, "Private Loans", NULL, 484.09, 06/23/2018, 1, NOW(), 0);
