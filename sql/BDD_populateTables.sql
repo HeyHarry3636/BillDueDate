0b83f214c15d93110d4fa17a99125ef438b4e94a
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-08 09:22:07 -0500

    Test development

--- sql/BDD_populateTables.sql
+++ sql/BDD_populateTables.sql
@@ -15,9 +15,9 @@ INSERT INTO tbl_bank(bank_id, user_id, bank_currentAmount, bank_payDayAmount, re
 	VALUES(1, 1, 1234.56, 1000.00, 1);
 
 -- Populate BILL table
-INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_date, recur_id, bill_createdDate, bill_paid)
+INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_autoWithdrawal, bill_date, recur_id, bill_createdDate, bill_paid)
 	VALUES(1, 1, "Fed Loans", "Income-Based", 199.70, 1, '2018-06-01', 1, NOW(), 1);
-INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_date, recur_id, bill_createdDate, bill_paid)
+INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_autoWithdrawal, bill_date, recur_id, bill_createdDate, bill_paid)
 	VALUES(2, 1, "Public Storage", NULL, 118.00, 1, '2018-06-01', 1, NOW(), 1);
-INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_date, recur_id, bill_createdDate, bill_paid)
+INSERT INTO tbl_bill(bill_id, user_id, bill_name, bill_description, bill_amount, bill_autoWithdrawal, bill_date, recur_id, bill_createdDate, bill_paid)
 	VALUES(3, 1, "Private Loans", NULL, 484.09, 0, '2018-06-01', 1, NOW(), 0);
