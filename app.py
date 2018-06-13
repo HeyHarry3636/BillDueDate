7092ba7b3ea5ddc3b789814cfaab4ee51fdb3dc2
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-13 08:35:18 -0500

    Test development

--- app.py
+++ app.py
@@ -107,6 +107,11 @@ def userHome():
 	else:
 		return render_template('error.html', error = 'Unauthorized Access')
 
+@app.route('/logout')
+def logout():
+	session.pop('user', None)
+	return redirect('/')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=True)
 
