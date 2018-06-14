2b14cc0e7567a38511a4bcc4a9556239090c6861
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-14 09:14:14 -0500

    Test Development

--- app.py
+++ app.py
@@ -171,21 +171,21 @@ def getBill():
 			
 			return(data)
 			# Prase data and convert to dictionary to return easily as JSON
-			bill_dict = []
-			for bill in data:
-				bill_item = {
-					'bill_id': bill[0],
-					'user_id': bill[1],
-					'bill_name': bill[2],
-					'bill_description': bill[3],
-					('bill_amount': decimal.Decimal(bill[4]), class=DecimalEncoder),
-					'bill_autoWithdrawal': bill[5],
-					'bill_date': bill[6],
-					'recur_id': bill[7],
-					'bill_createdDate': bill[8],
-					'bill_paid': bill[9]
-				}
-				bill_dict.append(bill_item)
+			# bill_dict = []
+			# for bill in data:
+				# bill_item = {
+					# 'bill_id': bill[0],
+					# 'user_id': bill[1],
+					# 'bill_name': bill[2],
+					# 'bill_description': bill[3],
+					# ('bill_amount': decimal.Decimal(bill[4]), class=DecimalEncoder),
+					# 'bill_autoWithdrawal': bill[5],
+					# 'bill_date': bill[6],
+					# 'recur_id': bill[7],
+					# 'bill_createdDate': bill[8],
+					# 'bill_paid': bill[9]
+				# }
+				# bill_dict.append(bill_item)
 			
 			#return json.dumps(bill_dict)
 			
