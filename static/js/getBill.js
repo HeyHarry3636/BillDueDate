2057f25e943c613c626b45779626aa73b8acb9e2
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-14 08:08:25 -0500

    Test Development

--- static/js/getBill.js
+++ static/js/getBill.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$.ajax({
 		url: '/getBill',
-		type: 'GET'
+		type: 'GET',
 /* 		success: function(response) {
 			console.log(response);
-		},
+		}, */
 		error: function(error) {
 			console.log(error);
-		} */
+		}
 	});
 });
