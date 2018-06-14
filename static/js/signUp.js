c5a4c46244e63b2227553014c7c2a3d1c2b7524a
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-14 11:46:11 -0500

    Test Development

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -8,7 +8,7 @@ $(function() {
                 console.log(response);
             }, */
 			success: function(response) {
-                window.location.href=console(log(response.redirect));
+                window.location.href=console.log(response.redirect);
             },
             error: function(error) {
                 console.log(error);
