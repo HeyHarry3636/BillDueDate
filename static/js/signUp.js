1f5b07412be876c1f30cbf8e8c024d36be20e735
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-19 10:40:33 -0500

    Test development

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -4,11 +4,8 @@ $(function() {
             url: '/signUp',
             data: $('form').serialize(),
             type: 'POST',
-/*             success: function(response) {
+            success: function(response) {
                 console.log(response);
-            }, */
-			      success: function(response) {
-                window.location.href=console.log(response.redirect);
             },
             error: function(error) {
                 console.log(error);
