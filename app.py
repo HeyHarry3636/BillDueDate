fe02a9ef58cfd71acb11a28c192eb4d0138b8946
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-20 07:47:39 -0500

    Test development

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ class RegisterForm(Form):
 def register():
 	form = RegisterForm(request.form)
 	if request.method == 'POST' and form.validate:
-
+		return render_template('register.html', form=form)
 	return render_template('register.html', form=form)
 
 if __name__ == '__main__':
