8ea9c70d6e40c9b2ab97e5a70452cf77727d7b4d
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-21 08:00:39 -0500

    Test development

--- app.py
+++ app.py
@@ -90,5 +90,9 @@ def register():
 	# 	if 'conn' in locals():
 	# 		conn.close()
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=True)
