1b7e3d8602b74219220b6fa4cbd95233e2fa7eec
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-21 07:57:32 -0500

    Test development

--- app.py
+++ app.py
@@ -55,40 +55,40 @@ class RegisterForm(Form):
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	try:
-		form = RegisterForm(request.form)
-		if request.method == 'POST' and form.validate():
-			_email = form.email.data
-			_password = form.password.data
+	# try:
+	form = RegisterForm(request.form)
+	if request.method == 'POST' and form.validate():
+		_email = form.email.data
+		_password = form.password.data
 
-			# Hash password with bcrypt
-			_e_password = _password.encode("utf-8")
-			_hs_password = bcrypt.hashpw(_e_password, bcrypt.gensalt())
+		# Hash password with bcrypt
+		_e_password = _password.encode("utf-8")
+		_hs_password = bcrypt.hashpw(_e_password, bcrypt.gensalt())
 
-			# Create mysql connection, create cursor, call procedure, fetch results
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.callproc('sp_createUser', (_email, _hs_password))
-			data = cursor.fetchall()
+		# Create mysql connection, create cursor, call procedure, fetch results
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.callproc('sp_createUser', (_email, _hs_password))
+		data = cursor.fetchall()
 
-			# Return successful or error message to see if called_proc worked
-			if len(data) is 0:
-				conn.commit()
-				flash('You have signed up!', 'success')
-				return redirect(url_for('login'))
-			else:
-				return render_template('error.html', error = str(data[0]))
+		# Return successful or error message to see if called_proc worked
+		if len(data) is 0:
+			conn.commit()
+			flash('You have signed up!', 'success')
+			return redirect(url_for('login'))
 		else:
-			return render_template('error.html', error = "Enter the required fields!")
+			return render_template('error.html', error = str(data[0]))
+	else:
+		return render_template('error.html', error = "Enter the required fields!")
 
-	except Exception as e:
-		return render_template('error.html', error = str(e))
-
-	finally:
-		if 'cursor' in locals():
-			cursor.close()
-		if 'conn' in locals():
-			conn.close()
+	# except Exception as e:
+	# 	return render_template('error.html', error = str(e))
+	#
+	# finally:
+	# 	if 'cursor' in locals():
+	# 		cursor.close()
+	# 	if 'conn' in locals():
+	# 		conn.close()
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=True)
