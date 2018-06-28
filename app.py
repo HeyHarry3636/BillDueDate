2e03ff57d538f0937ffeaf6be3f1dc803286622e
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-28 10:58:57 -0500

    Test Development

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, json, session, redirect, url_for, flash, logging, request
 from flaskext.mysql import MySQL
-from data import Bills
+#from data import Bills
 from wtforms import Form, validators, PasswordField, StringField, BooleanField, SelectField, DateField
 from wtforms.fields.html5 import EmailField, DecimalField, DateField
 from functools import wraps #Used for 'is_logged_in' var for dashboard
@@ -25,19 +25,19 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 # Grab temp test data from data.py file
-bill_dict = Bills()
+#bill_dict = Bills()
 
 @app.route('/')
 def index():
 	return render_template('home.html')
 
-@app.route('/mainBills')
-def mainBills():
-	return render_template('mainBills.html')
+# @app.route('/mainBills')
+# def mainBills():
+# 	return render_template('mainBills.html')
 
-@app.route('/tempData')
-def tempData():
-	return render_template('tempData.html', bills = bill_dict)
+# @app.route('/tempData')
+# def tempData():
+# 	return render_template('tempData.html', bills = bill_dict)
 
 @app.route('/bill/<string:id>/')
 def bill(id):
