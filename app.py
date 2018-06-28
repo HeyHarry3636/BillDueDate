08faea43485845acb360ef2f7e310223c7bef82c
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-28 09:37:03 -0500

    Test Development

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from wtforms import Form, validators, PasswordField, StringField, BooleanField, 
 from wtforms.fields.html5 import EmailField, DecimalField, DateField
 from functools import wraps #Used for 'is_logged_in' var for dashboard
 import bcrypt
+import datetime
 
 
 # Setup app and mysql instances
