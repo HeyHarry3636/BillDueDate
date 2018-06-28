8ea61d773c82b090b9b0e0145a508cf52bd364ca
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-28 07:20:20 -0500

    Test Development

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 from flask import Flask, render_template, json, session, redirect, url_for, flash, logging, request
 from flaskext.mysql import MySQL
 from data import Bills
-from wtforms import *
-from wtforms.fields.html5 import EmailField, DecimalField
+from wtforms import PasswordField, StringField, BooleanField
+from wtforms.fields.html5 import EmailField, DecimalField, DateField
 from functools import wraps #Used for 'is_logged_in' var for dashboard
 import bcrypt
 
