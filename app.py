b865594a920df2338ee41a94a04ab5a554068ac3
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-06-29 11:43:51 -0500

    Test Development

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from wtforms.fields.html5 import EmailField, DecimalField, DateField
 from functools import wraps #Used for 'is_logged_in' var for dashboard
 import bcrypt
 
-from forms import RegisterForm
+import forms # Custom forms.py file
 
 # Setup app and mysql instances
 app = Flask(__name__)
