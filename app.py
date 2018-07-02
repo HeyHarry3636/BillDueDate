5a7f826200af638a606076bf307db911e38590f3
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-02 12:36:49 -0500

    Test Development

--- app.py
+++ app.py
@@ -356,7 +356,7 @@ def testing():
 @app.route('/testUpdate', methods=['GET', 'POST'])
 def testUpdate():
 
-	_bank_id = request.form['bank_id']
+	_bank_id = form.bank_id.bankInfo
 	_user_id = session.get('user_id')
 	_bank_currentAmount = request.form['bank_currentAmount']
 	_bill_payDayAmount = request.form['bill_payDayAmount']
