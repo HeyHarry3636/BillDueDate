bd8648346255afdec369978aef4463ddc9b2b520
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-02 19:49:06 -0500

    Test Development

--- app.py
+++ app.py
@@ -175,7 +175,8 @@ def dashboard():
 		cursor.callproc('sp_getBankByUser', (_user_id,))
 		bankData = cursor.fetchall()
 
-		bill_dict.sort(key=sortBillDates)
+		sorted(bill_dict, key=sortBillDates)
+		#bill_dict.sort(key=sortBillDates)
 		for bill in bill_dict:
 			app.logger.info(bill[0])
 
