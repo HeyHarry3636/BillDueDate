93bcfa45bade0bac285b2fb88ced18012fd782e4
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-03 13:43:25 -0500

    Test development

--- globalVars.py
+++ globalVars.py
@@ -5,22 +5,19 @@ class cl_HasBankInformation():
 
     def __init__(self, bankInfoDoesExist):
         self.bankInfoDoesExist = bankInfoDoesExist
-        print("hasbankinfo class created")
+        print("hasBankInfo class created")
 
     def setBankInformation(self, hasBankInfo):
         if hasBankInfo is True:
             print("setterTrue")
             bankInfoDoesExist = True
-            return
         elif hasBankInfo is False:
             print("setterFalse")
             bankInfoDoesExist = False
-            return
         else:
             print("setterNone")
             bankInfoDoesExist = None
-            return
 
     def getBankInformation(self):
         print("getter")
-        return bankInfoDoesExist
+        return self.bankInfoDoesExist
