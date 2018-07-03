94789b5bb746e8b9a91ad2809a65d92ae170b405
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-03 10:49:45 -0500

    Test development

--- app.py
+++ app.py
@@ -425,11 +425,11 @@ def addBank():
 
 	if request.method == 'GET':
 		if hasBankData == False:
-			app.logger.info(hasBankData)
+			app.logger.info("if"+hasBankData)
 			return render_template('addBank.html', form=form)
 		else:
 			flash('You already have bank information entered', 'danger')
-			app.logger.info(hasBankData)
+			app.logger.info("else"+hasBankData)
 			return redirect(url_for('dashboard', form=form, hasBankData=hasBankData))
 
 	# try:
