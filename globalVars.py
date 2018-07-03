ab8b71ee36f6ff282c2f118ce64c6e1d788517ba
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-03 13:22:07 -0500

    Test development

--- globalVars.py
+++ globalVars.py
@@ -4,6 +4,7 @@ class cl_HasBankInformation():
     bankInfoDoesExist = None
 
     def __init__(self):
+        print("hasbankinfo class created")
 
     def setBankInformation(hasBankInfo):
         if hasBankInfo is True:
