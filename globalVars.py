cb1c3836d4199f93a2c05780ba8a2fa908d9a251
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-03 11:06:26 -0500

    Test development

--- globalVars.py
+++ globalVars.py
@@ -1,6 +1,7 @@
 
 
-class hasBankInformation(hasBankInfo):
+#class hasBankInformation(hasBankInfo):
+def hasBankInformation(hasBankInfo):
     if hasBankInfo is True:
         return True
     else:
