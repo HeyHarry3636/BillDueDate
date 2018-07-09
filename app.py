8ee26eb496be13aa360ae8a966ee6352a9a44ec9
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-07-09 12:22:09 -0500

    Test development

--- app.py
+++ app.py
@@ -168,9 +168,9 @@ def dashboard():
 			# Parse data and convert to dictionary to return easily as JSON
 			bill_dict_notSorted = []
 			for bill in billData:
-				print("running total1 " + str(bill) + " " + str(runningTotal.getRunningTotal()))
+				print("running total1 " + str(bill[0]) + " " + str(runningTotal.getRunningTotal()))
 				runningTotal.setRunningTotal(bill[4])
-				print("running total2 = " + str(bill) + " " + str(runningTotal.getRunningTotal()))
+				print("running total2 = " + str(bill[0]) + " " + str(runningTotal.getRunningTotal()))
 				bill_item = {
 					'bill_id': bill[0],
 					'user_id': bill[1],
