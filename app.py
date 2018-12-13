f2e09d0262004666672406f4f0ca9d92549f7812
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-12-13 12:05:59 -0600

    test development

--- app.py
+++ app.py
@@ -604,8 +604,11 @@ def createPayDayList():
 	cursor = conn.cursor()
 
 	# Check to see if there is already a bank account in the database
-	payDay = cursor.execute('SELECT bank_nextPayDate FROM tbl_bank WHERE user_id = %s', (_user_id))
-	app.logger.info("payDayTESTStr= " + str(payDay))
+	#payDay = cursor.execute('SELECT bank_nextPayDate FROM tbl_bank WHERE user_id = %s', (_user_id))
+	cursor.execute('SELECT bank_nextPayDate FROM tbl_bank WHERE user_id = %s', (_user_id))
+	payDay = cursor.fetchone()
+
+	app.logger.info("payDayTestStr= " + str(payDay))
 	#app.logger.info("payDay = " + payDay)
 	print("payDayDateStr = " + str(payDay))
 	#print("payDayDate = " + payDay)
