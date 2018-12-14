092e01db51b22b97283544bc93e5c7b03b446a1c
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-12-14 10:42:07 -0600

    test development

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def dashboard():
 			# Check to see if there is already a bank account in the database
 			cursor.execute('SELECT bank_nextPayDate FROM tbl_bank WHERE user_id = %s', (_user_id))
 			payDay = cursor.fetchone()
-			app.logger.info("payDay = " + str(payDay[0]))
+			app.logger.info("payDay = " + str(payDay[0].date()))
 
 			# Set initial payday date
 			runningDate.setInitialDate(payDay[0])
