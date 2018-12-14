fa0b9c4188f7e046fc3129b1b0c197cd81bc501c
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2018-12-14 11:12:20 -0600

    test development

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ runningTotal = globalVars.cl_calculateRunningTotal(0.00)
 # Create hasBankData class variable to let dashboard know if the user has bank information already
 hasBankData = globalVars.cl_HasBankInformation(False)
 # Create runningTotaldate so we can calculate when bills ARE
-runningDate = globalVars.cl_calculateRunningDate(datetime.date(1970, 1, 1))
+runningDate = globalVars.cl_calculateRunningDate(datetime.date(1970, 1, 1)).date()
 print("type(runningDate) = " + str(type(runningDate)))
 # runningDate = globalVars.cl_calculateRunningDate(datetime.datetime.date(1970, 1, 1))
 # print("type(runningDate) = " + str(type(runningDate)))
