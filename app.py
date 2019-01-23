a67d115b5b820fab0a741ddd7c0c05b6c46de26b
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-01-23 14:05:01 -0600

    Test development

--- app.py
+++ app.py
@@ -287,13 +287,14 @@ def dashboard():
 					# These bills occur after the current payday, so increment to the next payDay in the list
 					else:
 						print("else loop")
-						payDayListIndex = payDayListIndex + 1
+						#payDayListIndex = payDayListIndex + 1
 
-						if li['bill_date'] <= payDayList[payDayListIndex].date():
+						if li['bill_date'] <= payDayList[payDayListIndex+1].date():
 							print("2nd if loop")
 							print("2nd li['bill_date'] = " + str(li['bill_date']))
 							print("2nd payDayList[payDayListIndex] = " + str((payDayList[payDayListIndex]).date()))
 							runningTotal.setRunningTotal(li['bill_amount'])
+							payDayListIndex = payDayListIndex + 1
 
 
 
