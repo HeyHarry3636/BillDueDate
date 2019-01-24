467a96d2fb199d0b64c17fedbf83410a010c638b
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-01-23 18:07:54 -0600

    Test development

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def dashboard():
 									print("if")
 									print("x = " + str(x))
 									payAmount = 1774.16 * counter
-									runningTotal.setRunningTotalAfterPayDayMultiple(li['bill_amount'], 2)
+									runningTotal.setRunningTotalAfterPayDayMultiple(li['bill_amount'], x-2)
 									runningTotal.setRunningTotal(li['bill_amount'])
 									print("payAmount = " + str(payAmount))
 									break
