d852939cdd251e2caa07db666edfabbff178ed85
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-01-24 14:07:19 -0600

    bill paid development

--- static/js/saveCheckboxes.js
+++ static/js/saveCheckboxes.js
@@ -15,8 +15,15 @@ $(document).ready(function() {
   $('#has_been_paid').mouseup(function () {
       var answer = $("#has_been_paid").val();
 
+      $(".testCheckbox").each(function () {
+           var id = $(this).attr("id");
+
+           alert("Do something for: " + id + ", " + answer);
+           console.log("Do something for: " + id + ", " + answer);
+       });
+
       // alert("Do something for: " + answer);
-      console.log("Do something for: " + answer);
+      // console.log("Do something for: " + answer);
 
   });
 
