9f7c5a8165f86caadf20c9947dd59840de3f7886
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-01-24 12:46:27 -0600

    bill paid development

--- static/js/saveCheckboxes.js
+++ static/js/saveCheckboxes.js
@@ -1,6 +1,6 @@
 var sList = "";
 $('input[type=checkbox]').each(function () {
-    var sThisVal = (this.checked ? "1" : "0");
+    var sThisVal = ($(this).checked ? "1" : "0");
     sList += (sList=="" ? sThisVal : "," + sThisVal);
 });
 console.log (sList);
