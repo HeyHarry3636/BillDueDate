e07e09295fc6ce00e7b7df1c39fce032e79691fb
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-01-24 12:45:14 -0600

    bill paid development

--- static/js/saveCheckboxes.js
+++ static/js/saveCheckboxes.js
@@ -1,32 +1,6 @@
-$(document).ready(function() {
-  var sList = "";
-  $('input[type=checkbox]').each(function() {
-  // $('.saveCheckboxButton').is(':checked', function() {
-    // var checkboxStatus = $(this).val();
-
+var sList = "";
+$('input[type=checkbox]').each(function () {
     var sThisVal = (this.checked ? "1" : "0");
-
     sList += (sList=="" ? sThisVal : "," + sThisVal);
-
-
-
-    // req = $.ajax({
-    //   url : '/billsPaidCheckboxes',
-    //   type : 'POST',
-    //   data : { bill_billpaid : checkboxStatus }
-    // });
-
-    // // data = the passed back data from the app.py updateTest function
-    // req.done(function(data) {
-    //   $('#bankSection'+bank_id).fadeOut(1000).fadeIn(1000);
-    //   $('currentInput'+bank_id).val(data.bank_currentAmount);
-    //   $('payDayInput'+bank_id).val(data.bank_payDayAmount);
-    //   $('nextPayDateInput'+bank_id).val(data.bank_nextPayDate);
-    // });
-
-    // Do stuff for the bill rows loop through all columns
-
-
-  });
-  console.log(sList);
 });
+console.log (sList);
