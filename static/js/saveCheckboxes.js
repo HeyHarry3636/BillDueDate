4d515adee31ab3fe103a4ac3658a7fc20504b7e2
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-01-25 06:47:47 -0600

    billpaid test development

--- static/js/saveCheckboxes.js
+++ static/js/saveCheckboxes.js
@@ -1,3 +1,38 @@
+$(document).ready(function() {
+
+  // $('.updateButton, .bill-rows').on('click', function() {
+  $('.testCheckbox').mouseup(function() {
+    var bill_id = $(this).attr('bill_id');
+    console.log(bill_id)
+    var hasTheBillBeenPaid = $('#has_been_paid'+bill_id).val();
+    console.log(hasTheBillBeenPaid)
+
+    // req = $.ajax({
+    //   url : '/updateBankInfo',
+    //   type : 'POST',
+    //   data : { bank_currentAmount : currentAmount, bank_payDayAmount : payDayAmount }
+    // });
+    //
+    // // data = the passed back data from the app.py updateTest function
+    // req.done(function(data) {
+    //   $('#bankSection'+bank_id).fadeOut(1000).fadeIn(1000);
+    //   $('currentInput'+bank_id).val(data.bank_currentAmount);
+    //   $('payDayInput'+bank_id).val(data.bank_payDayAmount);
+    //   $('nextPayDateInput'+bank_id).val(data.bank_nextPayDate);
+    // });
+    //
+    // // Do stuff for the bill rows loop through all columns
+
+
+  });
+
+});
+
+
+
+
+
+
 // $(document).ready(function() {
 //
 //   $('#has_been_paid').mouseup(function () {
@@ -18,14 +53,14 @@
 // });
 
 
-$(document).ready(function() {
-
-  $('.testCheckbox').each(function () {
-      // var answer = $('has_been_paid').prop("checked", true)
-      var answer = $("#has_been_paid").val();
-      
-      // alert("Do something for: " + answer);
-      console.log("Do something for: " + answer);
-   });
-
-});
+// $(document).ready(function() {
+//
+//   $('.testCheckbox').each(function () {
+//       // var answer = $('has_been_paid').prop("checked", true)
+//       var answer = $("#has_been_paid").val();
+//
+//       // alert("Do something for: " + answer);
+//       console.log("Do something for: " + answer);
+//    });
+//
+// });
