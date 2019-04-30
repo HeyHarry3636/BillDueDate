f60c144a64cd8d0bade6c9d487722c9b39437571
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-04-29 19:17:55 -0500

    added dynamicTable.js

--- static/js/dynamicTable.js
+++ static/js/dynamicTable.js
@@ -1,3 +1,5 @@
+<button type="button" onclick="document.write(5)">Try it</button>
+
 $("a[id^=show_]").click(function(event) {
     $("#extra_" + $(this).attr('id').substr(5)).slideToggle("slow");
     event.preventDefault();
