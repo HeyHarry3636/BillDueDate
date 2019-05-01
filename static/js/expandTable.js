c73e06958c7d1c7854f4580871e6e47d48557185
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-05-01 18:39:31 -0500

    added dynamicTable.js

--- static/js/expandTable.js
+++ static/js/expandTable.js
@@ -1,11 +1,5 @@
-// $(document).ready(function() {
-//     $('[data-toggle="toggle"]').change(function(){
-//         $(this).parents().next('.hide').toggle();
-//     });
-// });
-
 $(document).ready(function() {
-    $('[data-toggle="toggle"]').delegate('label', 'click', function(){
+    $('[data-toggle="toggle"]').change(function(){
         $(this).parents().next('.hide').toggle();
     });
 });
