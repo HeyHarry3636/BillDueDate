c149464706668517b167955bf4c27a0af59e037d
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-05-01 18:55:55 -0500

    added dynamicTable.js

--- static/js/expandTable.js
+++ static/js/expandTable.js
@@ -6,10 +6,13 @@
 
 
 $(document).ready(function() {
-  var bill_id = $(this).attr('bill_id');
-  console.log("[expandTable.js] | bill_id = " + bill_id)
+  $('[data-toggle="toggle"]').mouseup(function() {
+    var bill_id = $(this).attr('bill_id');
+    console.log("[expandTable.js] | bill_id = " + bill_id)
+    $("#billDesc" + bill_id).hide();
 
-  $("#billDesc" + bill_id).click(function(){
-    $(this).hide();
+    // $("#billDesc" + bill_id).click(function(){
+    //   $(this).hide();
+    // });
   });
 });
