0885eaa54a85a1bf35721495084b578f1192e1fc
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-05-01 19:15:47 -0500

    added dynamicTable.js

--- static/js/expandTable.js
+++ static/js/expandTable.js
@@ -6,25 +6,22 @@
 
 
 $(document).ready(function() {
-  // $('#rowID'+bill_id).mouseup(function() {
 
-  $('[data-toggle="toggle"]').mouseup(function() {
+// FUNCTIONAL vv --------------------------------------------------
+  // $('[data-toggle="toggle"]').mouseup(function() {
+  //
+  //   var bill_id = $(this).attr('bill_id');
+  //   console.log("[expandTable.js] | bill_id = " + bill_id)
+  //
+  //   $('#rowID'+bill_id).hide()
+// FUNCTIONAL ^^ --------------------------------------------------
+var bill_id = $(this).attr('bill_id');
+console.log("[expandTable.js] | bill_id = " + bill_id)
 
-    var bill_id = $(this).attr('bill_id');
-    console.log("[expandTable.js] | bill_id = " + bill_id)
-
-    $('#rowID'+bill_id).hide()
+$('#rowID'+bill_id).mouseup(function() {
+  $(this).hide()
 
 
 
-    // var bill_id = $(this).attr('bill_id');
-    // console.log("[expandTable.js] | bill_id = " + bill_id)
-
-
-    // $('#billDesc'+bill_id).parents().next('.hide').toggle();
-
-    // $("#billDesc" + bill_id).click(function(){
-    //   $(this).hide();
-    // });
   });
 });
