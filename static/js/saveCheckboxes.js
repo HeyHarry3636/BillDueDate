11bd75c92b0d949fc2f871b5b8e1e12ad09ab24e
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-05-22 18:43:55 -0500

    added dynamicTable.js

--- static/js/saveCheckboxes.js
+++ static/js/saveCheckboxes.js
@@ -8,30 +8,29 @@ $(document).ready(function() {
     var hasTheBillBeenPaid = !$('#has_been_paid'+bill_id).is(':checked');
     console.log("[saveCheckboxes.js] | hasTheBillBeenPaid = " + hasTheBillBeenPaid)
 
-    // $('.bill-rowsTable').DataTable();
-    //
-    // req = $.ajax({
-    //   url : '/billsPaidCheckboxes',
-    //   type : 'POST',
-    //   data : { bill_billId : bill_id, bill_billPaid : hasTheBillBeenPaid }
-    // });
+    $('.bill-rowsTable').DataTable();
 
-    $('.bill-rowsTable').DataTable({
-      ajax: {
-        url : '/billsPaidCheckboxes',
-        type : 'POST',
-        data : { bill_billId : bill_id, bill_billPaid : hasTheBillBeenPaid }
-      }
+    req = $.ajax({
+      url : '/billsPaidCheckboxes',
+      type : 'POST',
+      data : { bill_billId : bill_id, bill_billPaid : hasTheBillBeenPaid }
     });
 
+    // $('.bill-rowsTable').DataTable({
+    //   ajax: {
+    //     url : '/billsPaidCheckboxes',
+    //     type : 'POST',
+    //     data : { bill_billId : bill_id, bill_billPaid : hasTheBillBeenPaid }
+    //   }
+    // });
     // $('.bill-rowsTable').DataTable().ajax.reload();
 
     // data = the passed back data from the app.py updateTest function
-    // req.done(function(data) {
-    //   $('.bills-rowsTable').load('.bills-rowsTable');
+    req.done(function(data) {
+      // $('.bills-rowsTable').load('.bills-rowsTable');
       // $('.bill-rowsTable').DataTable().ajax.reload();
 
-      // location.reload();
+      location.reload();
 
     });
 
