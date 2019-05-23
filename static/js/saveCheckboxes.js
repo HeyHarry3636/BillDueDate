a384c15d683f2fb36819fd8114f1dfc1f30b88ce
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-05-22 19:03:33 -0500

    added dynamicTable.js

--- static/js/saveCheckboxes.js
+++ static/js/saveCheckboxes.js
@@ -28,9 +28,9 @@ $(document).ready(function() {
     // data = the passed back data from the app.py updateTest function
     req.done(function(data) {
       // $('.bills-rowsTable').load('.bills-rowsTable');
-      $('.bill-rowsTable').DataTable().ajax.reload();
+      // $('.bill-rowsTable').DataTable().ajax.reload();
 
-      // location.reload();
+      location.reload();
 
     });
 
