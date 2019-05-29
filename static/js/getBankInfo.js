a0e0a11d15bef24aa2690040ac739210b8da3d93
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-05-29 09:35:06 -0500

    added dynamicTable.js

--- static/js/getBankInfo.js
+++ static/js/getBankInfo.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
       $('currentInput'+bank_id).val(data.bank_currentAmount);
       $('payDayInput'+bank_id).val(data.bank_payDayAmount);
       $('nextPayDateInput'+bank_id).val(data.bank_nextPayDate);
+      $('projectedMonths'+bank_id).val(data.bank_projectedMonths)
       location.reload();
     });
 
