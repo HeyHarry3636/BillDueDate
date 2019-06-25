60c248f8211c2292cd014b422af5f9d014acb414
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-06-25 16:47:51 -0500

    testing selectfield

--- app.py
+++ app.py
@@ -840,7 +840,7 @@ def testSelectField():
 
 
 		if request.method == 'POST':
-			cursor.execute('SELECT {} FROM city').format(formTest.city.id)
+			cursor.execute('SELECT %s FROM city', (formTest.city.id))
 			returnVal = cursor.fetchone()
 			print("returnVal = " + str(returnVal))
 			conn.commit()
