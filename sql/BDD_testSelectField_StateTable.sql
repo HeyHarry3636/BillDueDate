f21e656aeb9c05caacfb59880da121a0f3435aa1
HeyHarry3636 <20846736+HeyHarry3636@users.noreply.github.com>
2019-06-24 19:42:59 -0500

    testing selectfield

---
+++ sql/BDD_testSelectField_StateTable.sql
@@ -0,0 +1,25 @@
+USE BillDueDate
+
+-- Credentials for Test USER
+-- U: test
+-- P: Test1234!
+
+-- DROP all tables from previous uses
+DROP TABLE IF EXISTS city;
+
+-- Create the Cities table
+CREATE TABLE city (
+	id BIGINT NOT NULL AUTO_INCREMENT UNIQUE,
+	state VARCHAR(2) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO city(id, state, name)
+	VALUES(1, "NV", "Las Vegas");
+INSERT INTO city(id, state, name)
+	VALUES(2, "NV", "Reno");
+INSERT INTO city(id, state, name)
+  VALUES(3, "CA", "Los Angeles");
+INSERT INTO city(id, state, name)
+	VALUES(4, "CA", "San Diego");
